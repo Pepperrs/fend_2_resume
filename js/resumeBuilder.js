a541b189fb79678bede957ccffdf92aba0f0866e
Peter Schüllermann <Peter.schuellermann@gmail.com>
2016-01-28 15:38:11 +0100

    update resumeBuilder.js
    added projects.display()

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,51 +1,6 @@
 header = $("#header");
 main = $("#main");
 
-$(document).click(function (loc) {
-    console.log("pageX: " + loc.pageX + ", pageY: " + loc.pageY);
-});
-
-function displayBio() {
-    if (bio.skills.length != 0) {
-        header.append(HTMLskillsStart);
-        for (skill in bio.skills) {
-            if (bio.skills.hasOwnProperty(skill)) {
-                $("#skills").append(HTMLskills.replace("%data%", bio.skills[skill]));
-            }
-        }
-    }
-}
-
-function displayWork() {
-    for (job in work.jobs) {
-        if (work.jobs.hasOwnProperty(job)) {
-            $("#workExperience").append(HTMLworkStart);
-            var workentry = $(".work-entry:last");
-            var employer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-            var title = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-            workentry.append(title + employer);
-            var dates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-            workentry.append(dates);
-            var description = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-            workentry.append(description);
-            //var location = HTMLworkLocation.replace("%data%", work.jobs[job].location);
-            //$(".work-entry:last").append(location);
-        }
-    }
-
-}
-
-function displayProjects() {
-
-}
-
-function inName(name) {
-    name = name.trim().split(" ");
-    name[0] = name[0].slice(0, 1).toUpperCase() + name[0].slice(1).toLowerCase();
-    name[1] = name[1].toUpperCase();
-    return name.join(" ")
-}
-
 
 var bio = {
     "name": "Peter Schüllermann",
@@ -107,12 +62,71 @@ var projects = {
     "projects": [
         {
             "title": "RedJenkins",
-            "dates": 2,
+            "dates": "2015",
             "description": "connection between Redmine and Jenkins",
-            "images": ["images\197x148.gif"]
+            "images": ["images/197x148.gif"]
         }
     ]
 };
+
+$(document).click(function (loc) {
+    console.log("pageX: " + loc.pageX + ", pageY: " + loc.pageY);
+});
+
+function displayBio() {
+    if (bio.skills.length != 0) {
+        header.append(HTMLskillsStart);
+        for (skill in bio.skills) {
+            if (bio.skills.hasOwnProperty(skill)) {
+                $("#skills").append(HTMLskills.replace("%data%", bio.skills[skill]));
+            }
+        }
+    }
+}
+
+function displayWork() {
+    for (job in work.jobs) {
+        if (work.jobs.hasOwnProperty(job)) {
+            $("#workExperience").append(HTMLworkStart);
+            var workentry = $(".work-entry:last");
+            var employer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+            var title = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+            workentry.append(title + employer);
+            var dates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+            workentry.append(dates);
+            var description = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+            workentry.append(description);
+            //var location = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+            //$(".work-entry:last").append(location);
+        }
+    }
+
+}
+
+projects.display = function () {
+    for (project in projects.projects) {
+        if (projects.projects.hasOwnProperty(project)) {
+            $("#projects").append(HTMLprojectStart);
+            var projectEntry = $(".project-entry:last");
+            var title = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+            projectEntry.append(title);
+            var dates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+            projectEntry.append(dates);
+            var description = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+            projectEntry.append(description);
+            var images = HTMLprojectImage.replace("%data%", projects.projects[project].images);
+            projectEntry.append(images);
+        }
+    }
+}
+
+function inName(name) {
+    name = name.trim().split(" ");
+    name[0] = name[0].slice(0, 1).toUpperCase() + name[0].slice(1).toLowerCase();
+    name[1] = name[1].toUpperCase();
+    return name.join(" ")
+}
+
 
 header.append(HTMLheaderName.replace("%data%", bio.name));
 header.append(HTMLheaderRole.replace("%data%", bio.role));
@@ -121,3 +135,4 @@ displayBio();
 
 displayWork();
 
+projects.display();
